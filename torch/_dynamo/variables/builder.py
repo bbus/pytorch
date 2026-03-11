ca6015b6d85a79a3197f2381a5802583f744b9aa
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2026-03-11 16:47:44 +0100

    Fix failing test due to calling `__index__` on `SymNode`.

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -3446,7 +3446,8 @@ def handle_traced_output(
         ]
         or (
             # TODO: this is a little sus, because we didn't check what the self is
-            proxy.node.op == "call_method" and proxy.node.target == "bit_length"
+            proxy.node.op == "call_method"
+            and proxy.node.target in ("bit_length", "__index__")
         )
     ):
         set_example_value(proxy.node, example_value)
@@ -3497,6 +3498,7 @@ def handle_traced_output(
             fake_script_obj,
         )
     else:
+        breakpoint()
         unimplemented(
             gb_type="torch.* op returned non-Tensor",
             context=f"example_value type: {typestr(example_value)}; op: {proxy.node.op}; target: {proxy.node.target}",
